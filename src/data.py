dc90b9f2e752dc641dd5d720c75b4a845438a33a
Veronica <verveder@yandex.ru>
2023-10-17 09:53:06 +0300

    Update locators

--- src/data.py
+++ src/data.py
@@ -3,6 +3,6 @@ MAIN_URL = "https://cloud.ru/ru"
 APIGW_PRODUCT_URL = "https://cloud.ru/ru/products/oblachnyj-api-gateway-hosting"
 
 # XPath's locators for elements
-LOCATOR_SERVICES = "//*[@data-qa='header_nav_item'][text()='Сервисы']"
-LOCATOR_INFRASTRUCTURE = "//*[contains(@class, 'OZqKu')][text()='Инфраструктура']"
-LOCATOR_APIGW = "//h2[text()='Инфраструктура']/following-sibling::a[contains(@href, 'api-gateway')]"
+LOCATOR_SERVICES = "//*[@data-qa='header_nav_item'][text()='Продукты']"
+LOCATOR_INFRASTRUCTURE = "//*[contains(@class, 'OZqKu')][@id='layout-header-product-category-Инфраструктура']"
+LOCATOR_APIGW = "//*[@class='Products_title__W69uL'][text()='Инфраструктура']/following-sibling::div[@class='Products_blockGrid__yPVN9']/div/a[@id='header-card-link-API на базе технологий VMware']"
